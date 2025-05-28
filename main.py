4556e43ee8b32a290b38ddefb94741bcf3957501
Paul Han <paulh@dtric.com>
2025-05-27 16:05:07 -1000

    Basic example with local filesystem

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import jinja2 as jinja
+
+
 def main():
-    print("Hello from steady-grewp!")
+    env = jinja.Environment(
+        loader=jinja.FileSystemLoader("templates")
+    )
+    template = env.get_template('header.html')
+    print(template.render())
 
 
 if __name__ == "__main__":
